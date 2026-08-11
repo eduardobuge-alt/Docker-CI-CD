099b21a1438d5ce4132a8afd5a8daf8f15582a97
NetAcad DEVASC <changeme@example.com>
2026-08-11 19:04:52 +0000

    Docker y CI/CD

---
+++ sample_app.py
@@ -0,0 +1,12 @@
+# Add to this file for the sample app lab
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Sample App running con port 8888"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888)
